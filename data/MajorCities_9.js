51b3145334592e6c7b7ab5d92aeda4ecfe2871bc
dev-vw <36392137+dev-vw@users.noreply.github.com>
2018-12-05 11:11:16 -0800

    Add files via upload

---
+++ data/MajorCities_9.js
@@ -0,0 +1,12 @@
+var json_MajorCities_9 = {
+"type": "FeatureCollection",
+"name": "MajorCities_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 36.976843200738728, -7.40554389148757 ] } },
+{ "type": "Feature", "properties": { "Id": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 36.982196988001846, -7.656458054552376 ] } },
+{ "type": "Feature", "properties": { "Id": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 36.502273694356582, -7.468616502315214 ] } },
+{ "type": "Feature", "properties": { "Id": 3.0 }, "geometry": { "type": "Point", "coordinates": [ 36.683463487146561, -8.124343440817643 ] } },
+{ "type": "Feature", "properties": { "Id": 4.0 }, "geometry": { "type": "Point", "coordinates": [ 36.89859146087889, -7.862380440929155 ] } }
+]
+}
---
+++ data/StudyVillages_10.js
@@ -0,0 +1,10 @@
+var json_StudyVillages_10 = {
+"type": "FeatureCollection",
+"name": "StudyVillages_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 36.905729843896374, -7.835730477663852 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 36.520801741219231, -7.513869665454559 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 36.954099980143582, -7.808338189951491 ] } }
+]
+}
